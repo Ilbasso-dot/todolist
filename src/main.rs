03e076613b6ff31e0d761dff4010c5c28233c29d
Ilbasso-dot <baxiandri@gmail.com>
2023-04-30 13:58:42 +0200

    add helper and list on all commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::fs;
 
-fn write_file(v: Vec<(i32, i32, &str)>) {
+// help function
+fn help() {
+    println!("Usage: todolist [OPTION] [ARGUMENTS]");
+    println!("Options:");
+    // write all options
+    println!("\t-l\t List all tasks");
+    println!("\t-i\t Add a new task");
+    println!("\t-r\t Remove a task");
+}
+
+fn write_file(v: &Vec<(i32, i32, &str)>) {
     let mut s = String::new();
     for i in v {
         s.push_str(&format!("{} {} {}\n", i.0, i.1, i.2));
@@ -18,7 +28,7 @@ fn main() {
         fs::read_to_string("/home/ilbasso/Documents/myscript/todolist/src/test.txt").unwrap();
     let s: Vec<&str> = binding.as_str().split('\n').collect();
     // commands now contains all args after the executable name
-    println!("Args: {:?}", commands);
+    // println!("Args: {:?}", commands);
 
     let mut v: Vec<(i32, i32, &str)> = Vec::new();
 
@@ -35,8 +45,8 @@ fn main() {
     match commands[1].as_str() {
         "-l" => {
             v.sort_by(|a, b| a.0.cmp(&b.0));
-            for i in v {
-                println!("priority: {}, effort: {} =>  {}", i.0, i.1, i.2);
+            for (n, i) in v.iter().enumerate() {
+                println!("{}) priority: {}, effort: {} =>  {}", n, i.0, i.1, i.2);
             }
         }
         "-i" => {
@@ -47,23 +57,28 @@ fn main() {
             );
             v.push(new);
             v.sort_by(|a, b| a.0.cmp(&b.0));
-            write_file(v);
+            write_file(&v);
+            for (n, i) in v.iter().enumerate() {
+                println!("{}) priority: {}, effort: {} =>  {}", n, i.0, i.1, i.2);
+            }
         }
         "-r" => {
-            v.remove(0);
-            write_file(v);
-        }
-        "-d" => {
-            let mut i = 0;
-            for j in &v {
-                if j.0 == commands[2].parse::<i32>().unwrap() {
-                    break;
+            // if commands[2] exists, parse to int and remove it
+            if commands.len() > 2 {
+                let index = commands[2].parse::<i32>().unwrap();
+                if index < v.len() as i32 {
+                    v.remove(index as usize);
+                    write_file(&v);
+                    return;
                 }
-                i += 1;
             }
-            v.remove(i);
-            write_file(v);
+            v.remove(0);
+            write_file(&v);
+            for (n, i) in v.iter().enumerate() {
+                println!("{}) priority: {}, effort: {} =>  {}", n, i.0, i.1, i.2);
+            }
         }
+        "-h" => help(),
         _ => {}
     }
 
